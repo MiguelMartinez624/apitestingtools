e491ae8126949ca6385d19a392af225b2a8b9be3
miguel martinez <miguelmartinez@miguels-MacBook-Pro.local>
2023-02-08 16:27:01 -0400

    feat:expose request result

--- mockserver/request_result.go
+++ mockserver/request_result.go
@@ -2,6 +2,7 @@ package mockserver
 
 import (
 	"bytes"
+	"encoding/json"
 	"testing"
 )
 
@@ -22,4 +23,22 @@ func (receiver RequestResult) Asserts(t *testing.T, asserts ...AssertFunc) {
 	for _, assert := range asserts {
 		assert(t, receiver.output)
 	}
+}
+
+func (receiver RequestResult) Result[T interface{}]() T {
+	var result T
+	if err := json.NewDecoder(receiver.output.Body).Decode(&result); err != nil {
+		panic(err)
+	}
+
+	return T
+
+}
+
+func (receiver RequestResult) Code() int {
+	return receiver.output.Code
+}
+
+func (receiver RequestResult) String() string {
+	return receiver.output.BodyStr
 }
